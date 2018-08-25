9d60aaeafd1bf2caf39dafb11be5c30f3af94b3d
Kirin Patel <kirinpatel@gmail.com>
2018-08-25 18:12:29 -0400

    Fixed website to properly support mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,19 @@
     <div class="w-100 h-100 bg-dark">
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
-                <div class="card col-4 text-center justify-content-center">
+                <div class="card col-sm-12 col-md-4 text-center justify-content-center">
                     <div class="card-body">
                         <h5 class="card-title">Couple List</h5>
                         <p class="card-text">
                             A bucket list for couples;
                             <br>
-                            create, edit, or view yours
+                            create, edit, or view yours with the app.
                         </p>
                         <a href="https://itunes.apple.com/us/app/couple-list/id1310979486?ls=1&mt=8">
                             <img src="@/assets/download_iOS.svg"
                                  alt="Download on the App Store"
                                  height="40"
-                                 width="150">
+                                 width="100%">
                         </a>
                     </div>
                 </div>
--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -2,7 +2,7 @@
     <div class="w-100 h-100 bg-dark">
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
-                <div class="card col-4 text-center justify-content-center">
+                <div class="card col-sm-12 col-md-4 text-center justify-content-center">
                     <div class="card-body">
                         <h5 class="card-title">Privacy Policy</h5>
                         <p class="card-text">
