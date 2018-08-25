7e5b5b86c3305d91065778e52dc032aed3b5886a
Kirin Patel <kirinpatel@gmail.com>
2018-08-25 16:40:28 -0400

    Static html -> VueJS

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ul class="nav nav-tabs bg-dark">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">About</router-link>
+      </li>
+    </ul>
+    <router-view style="max-height: calc(100% - 63.42px);"/>
+  </div>
+</template>
+
+<style>
+  #app {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-100 h-100 bg-dark">
+    <div class="container h-100">
+      <div class="row h-100 justify-content-center align-items-center">
+        <div class="card col-4 text-center justify-content-center">
+          <div class="card-body">
+            <h5 class="card-title">Couple List</h5>
+            <p class="card-text">
+              A bucket list for couples;
+              <br>
+              create, edit, or view yours
+            </p>
+            <a href="#" class="btn btn-dark btn-raised">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
