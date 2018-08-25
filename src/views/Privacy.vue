1e2469c623d2de0e0840e9f7eeb721c4c9422085
Kirin Patel <kirinpatel@gmail.com>
2018-08-25 17:14:09 -0400

    Added Privacy screen

---
+++ src/views/Privacy.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-100 h-100 bg-dark">
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="card col-4 text-center justify-content-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Privacy Policy</h5>
+                        <p class="card-text">
+                            All user data and credentials are stored through Firebase a service offered by Google.
+                            Within Firebase, all data is secured through permissions that only allow the user to edit
+                            their information and their list. This data is <strong>NOT</strong> accessible to others and
+                            will <strong>NEVER</strong> be shared. Lists may only be viewed by those who are in it and are not accessible
+                            to others.
+                            <br>
+                            <br>
+                            No matter the content of your list, it is secure and private.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'privacy',
+};
+</script>
